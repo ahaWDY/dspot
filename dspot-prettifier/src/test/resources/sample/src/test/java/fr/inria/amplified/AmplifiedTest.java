b446c38095f043a3991f106a67414210afa48f98
Carolin Brandt <c.e.brandt@tudelft.nl>
2021-11-15 14:18:16 +0100

    adding two real amplified test cases + test-cube improved coverage output to prettifier test sample

--- dspot-prettifier/src/test/resources/sample/src/test/java/fr/inria/amplified/AmplifiedTest.java
+++ dspot-prettifier/src/test/resources/sample/src/test/java/fr/inria/amplified/AmplifiedTest.java
@@ -1,5 +1,7 @@
 package fr.inria.amplified;
 
+import eu.stamp_project.App;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -36,5 +38,24 @@ public class AmplifiedTest {
     public void amplifiedTestWithOldVariable() throws Exception {
         int testingAnInt = 5;
         assertEquals(5, testingAnInt);
+    }
+
+    @Test(timeout = 10000)
+    public void test1_mg12_assSep41() throws Exception {
+        int __DSPOT_j_0 = -1150482841;
+        App app = new App(4);
+        app.compute(__DSPOT_j_0);
+        Assert.assertEquals(-153482034, ((int) (((App) (app)).getInt())));
+    }
+
+    @Test(timeout = 10000)
+    public void test1_mg13_failAssert0() throws Exception {
+        try {
+            App app = new App(4);
+            app.throwException();
+            Assert.fail("test1_mg13 should have thrown IllegalArgumentException");
+        } catch (IllegalArgumentException expected) {
+            Assert.assertEquals("Illegal Arg", expected.getMessage());
+        }
     }
 }
