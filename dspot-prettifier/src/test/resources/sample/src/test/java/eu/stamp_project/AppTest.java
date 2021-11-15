188c7e829417cc5121f2b8c9d874c064ece6da01
Carolin Brandt <c.e.brandt@tudelft.nl>
2021-11-15 15:13:52 +0100

    reuse dspot initialization and respec configured test class / casses

--- dspot-prettifier/src/test/resources/sample/src/test/java/eu/stamp_project/AppTest.java
+++ dspot-prettifier/src/test/resources/sample/src/test/java/eu/stamp_project/AppTest.java
@@ -30,4 +30,23 @@ public class AppTest {
             Assert.assertEquals("Illegal Arg", expected.getMessage());
         }
     }
+
+    @Test(timeout = 10000)
+    public void test1_mg12_assSep41() throws Exception {
+        int __DSPOT_j_0 = -1150482841;
+        App app = new App(4);
+        app.compute(__DSPOT_j_0);
+        Assert.assertEquals(-153482034, ((int) (((App) (app)).getInt())));
+    }
+
+    @Test(timeout = 10000)
+    public void test1_mg13_failAssert0() throws Exception {
+        try {
+            App app = new App(4);
+            app.throwException();
+            Assert.fail("test1_mg13 should have thrown IllegalArgumentException");
+        } catch (IllegalArgumentException expected) {
+            Assert.assertEquals("Illegal Arg", expected.getMessage());
+        }
+    }
 }
