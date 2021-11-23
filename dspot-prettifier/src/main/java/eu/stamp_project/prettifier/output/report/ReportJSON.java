88843bdf013134f07d83327251aab54920d122b1
Carolin Brandt <c.e.brandt@tudelft.nl>
2021-11-23 11:20:31 +0100

    fix prettifier issues to run it in experiment

--- dspot-prettifier/src/main/java/eu/stamp_project/prettifier/output/report/ReportJSON.java
+++ dspot-prettifier/src/main/java/eu/stamp_project/prettifier/output/report/ReportJSON.java
@@ -1,10 +1,20 @@
 package eu.stamp_project.prettifier.output.report;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import eu.stamp_project.dspot.common.report.output.selector.TestClassJSON;
+import eu.stamp_project.prettifier.Main;
 import eu.stamp_project.prettifier.Util;
 import eu.stamp_project.prettifier.configuration.UserInput;
 import eu.stamp_project.prettifier.output.report.minimization.general.GeneralMinimizationJSON;
 import eu.stamp_project.prettifier.output.report.minimization.pit.PitMinimizationJSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spoon.reflect.declaration.CtType;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * created by Benjamin DANGLOT
@@ -12,6 +22,8 @@ import eu.stamp_project.prettifier.output.report.minimization.pit.PitMinimizatio
  * on 25/02/19
  */
 public class ReportJSON {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReportJSON.class);
 
     public GeneralMinimizationJSON generalMinimizationJSON;
 
@@ -31,4 +43,17 @@ public class ReportJSON {
         this.amplificationReport = Util.readExtendedCoverageResultJSON(configuration);
     }
 
+    public void output(UserInput configuration, CtType<?> amplifiedTestClass) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        final String pathname =
+                configuration.getOutputDirectory() + File.separator + amplifiedTestClass.getQualifiedName() +
+                "_prettifier_report.json";
+        LOGGER.info("Output a report in {}", pathname);
+        final File file = new File(pathname);
+        try (FileWriter writer = new FileWriter(file, false)) {
+            writer.write(gson.toJson(Main.report));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
