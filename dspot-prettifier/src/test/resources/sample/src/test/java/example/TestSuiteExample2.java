cf0a104091a6946a2aa1af3c7ecd11a209331553
Carolin Brandt <c.e.brandt@tudelft.nl>
2021-12-09 16:38:57 +0100

    test data for adavanced test descriptions

---
+++ dspot-prettifier/src/test/resources/sample/src/test/java/example/TestSuiteExample2.java
@@ -0,0 +1,16 @@
+package example;
+import org.junit.Assert;
+import org.junit.Test;
+public class TestSuiteExample2 {
+    private static int integer = TestResources.integer;
+
+    @Test(timeout = 10000)
+    public void test2_mg63_assSep152() throws Exception {
+        int __DSPOT_index_9 = -839241703;
+        String __DSPOT_s_8 = "?i!rb0/|]6^FT)-ef&bk";
+        Example ex = new Example();
+        char findChar = ex.charAt("abcd", 3);
+        char o_test2_mg63__7 = ex.charAt(__DSPOT_s_8, __DSPOT_index_9);
+        Assert.assertEquals('?', o_test2_mg63__7);
+    }
+}
